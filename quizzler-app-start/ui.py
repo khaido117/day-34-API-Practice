420e0805d1a36f1c65033f34c7a52c9f6ec79ef5
Khai Do <khaitroyy@gmail.com>
2023-10-05 20:13:21 -0500

    update GUI

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -1,2 +1,36 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self, quiz_brain):
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row= 0)
+
+        self.canvas = Canvas(width=300, height=250, background= "white", highlightthickness=0)
+        self.question_text = self.canvas.create_text(
+            150, 125,
+            text="Some Questions",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic"))
+        
+
+        self.canvas.grid(column=0, row= 1, columnspan=2, pady=50)
+        
+        true_image = PhotoImage(file="/Users/khaido/Library/CloudStorage/GoogleDrive-khaitroyy@gmail.com/My Drive/Code/day-34-API-Practice/quizzler-app-start/images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(column=1, row=2)
+
+        false_image = PhotoImage(file="/Users/khaido/Library/CloudStorage/GoogleDrive-khaitroyy@gmail.com/My Drive/Code/day-34-API-Practice/quizzler-app-start/images/false.png")
+        self.true_button = Button(image=false_image, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        self.window.mainloop()
+
+    def next_question(self)
